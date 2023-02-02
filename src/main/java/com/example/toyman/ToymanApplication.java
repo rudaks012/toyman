03bbd49b336931f25b9f4de32f2a4cf65dfed79f
gyeongman <rudaks012@naver.com>
2023-02-03 00:02:03 +0900

    Initial commit

---
+++ src/main/java/com/example/toyman/ToymanApplication.java
@@ -0,0 +1,14 @@
+package com.example.toyman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToymanApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        SpringApplication.run(ToymanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toyman/ToymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToymanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
